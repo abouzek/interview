37331976c19805608c6cacc10dacc09b7941a319
abouzek <alan.bouzek@gmail.com>
2016-02-08 13:32:18 -0600

    Add NSAllowArbitraryLoads = YES key to Info.plist Update TODO warning directives in ContactsService.m Add example response to ContactsService.m

--- Interview/ContactsService.m
+++ Interview/ContactsService.m
@@ -11,9 +11,30 @@
 
 @implementation ContactsService
 
+/**
+ * Example response from http://mocky.io/v2/55c0d0632d3a3a4c10032c45:
+ *
+ * [
+ *      {
+ *          "firstName": "Alan",
+ *          "lastName": "Bouzek",
+ *          "phoneNumber": "555-555-5555",
+ *          "address": "1111 Park Place"
+ *      },
+ *      {
+ *          "firstName": "Test",
+ *          "lastName": "User",
+ *          "phoneNumber": "111-111-1111",
+ *          "address": "1234 Boardwalk"
+ *      }
+ * ]
+ *
+ */
+
 -(void)getContactsWithCompletionBlock:(void(^)(NSArray *contacts))completionBlock {
-#warning - TODO: Make a GET request to http://www.mocky.io/v2/55c0d0632d3a3a4c10032c45 and parse the Contact objects from the returned JSON into the contacts NSArray
-#warning - TODO: Call completionBlock with the parsed NSArray of Contact objects
+#warning - TODO: Make a GET request to http://www.mocky.io/v2/55c0d0632d3a3a4c10032c45
+#warning - TODO: Parse the returned JSON into an NSArray containing Contact objects (Contact has a convenience constructor to make this easier)
+#warning - TODO: Call completionBlock with the NSArray of Contact objects as an argument
 }
 
 @end
